7cfe2ba81289cbd166521185af1dce09b1fa58df
heeyupp <130219670+heeyupp@users.noreply.github.com>
2024-03-18 14:22:02 +0900

    Title: 합을 복사하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240318/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a =1 ;
+    int b = 2;
+    int c =3; 
+    a=b=c= a+b+c;
+    std :: cout << a <<" "<<b<<" "<<c;
+    return 0;
+}
