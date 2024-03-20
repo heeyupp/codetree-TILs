9d1045fc13734c679fcbf82a99c28e6e84ad0d4c
heeyupp <130219670+heeyupp@users.noreply.github.com>
2024-03-20 13:45:43 +0900

    Title: 실수 받아 그대로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240320/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a; 
+    std:: cin >> a ; 
+    std::cout <<std:: fixed; 
+    std:: cout.precision(2);
+    std :: cout << a;
+    return 0;
+}
